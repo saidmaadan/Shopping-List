fbe74b7897c68aecdf61449a5525f66f65e163d5
Said Maadan <me@saidmaadan.com>
2014-04-02 21:57:22 +0100

    Create user interface

--- css/app.js
+++ css/app.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
+    $('#list-item, button').click(function() {
+    	var toAdd = $("input[name=list-item]").val();
+        $('#listed-item').append("<p>"+toAdd+"</p>");
+    });
 });
