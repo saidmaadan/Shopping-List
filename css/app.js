8b6c68efec2c62147f54cdcd31fce674e310feab
Said Maadan <me@saidmaadan.com>
2014-04-07 15:48:06 -0400

    Modified app.js and style.css

--- css/app.js
+++ css/app.js
@@ -1,6 +1,34 @@
-$(document).ready(function() {
-    $('#list-item, button').click(function() {
-    	var toAdd = $("input[name=list-item]").val();
-        $('#listed-item').append("<p>"+toAdd+"</p>");
-    });
+$(document).ready(function(){
+	$('button').click(function(){
+		item = $('#list-item').val();
+		$("#listed-item").append('<div class="added-item"><img src = "img/deleted.png">'+item+'</div>');
+			listItemClick();
+			$(this).val("");
+	});
+	$("#list-item").keyup(function(e){
+		if (e.which == 13) {
+			item = $('#list-item').val();
+			$("#listed-item").append('<div class="added-item"><img src = "img/deleted.png">'+item+'</div>');
+			listItemClick();
+			$(this).val("");
+		}
+	});
+	listItemClick();
 });
+
+listItemClick = function(){
+	$(".added-item").unbind("button");
+	$(".added-item").unbind("click");
+	$(".added-item").on('click', function(){
+		if ($(this).hasClass("remove")){
+			$(this).removeClass("remove").find("img").hide();
+		}
+		else{
+			$(this).addClass("remove").find("img").show();
+		}
+	});
+	$('img').unbind('click');
+	$('img').on('click', function(){
+		$(this).closest(".added-item").remove();
+	});
+}
